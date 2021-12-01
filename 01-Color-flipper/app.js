406a29f2330cc8a3184275824e977e8025ab294d
ramazankarisan <rkarisan25@gmail.com>
2021-12-01 08:43:44 +0100

    index.html and app.js created

---
+++ 01-Color-flipper/app.js
@@ -0,0 +1,19 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color')
+
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * colors.length)
+}
+
+btn.addEventListener('click', (e) => {
+
+  // get random number between 0-3
+
+  const randomNumber = getRandomNumber();
+
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+
+})
